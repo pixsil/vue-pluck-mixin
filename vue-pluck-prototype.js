34075bac1fb58575cf5fad7bfddc88b4c4314162
pixsil <57232302+pixsil@users.noreply.github.com>
2022-04-08 15:09:06 +0200

    Update vue-pluck-prototype.js

--- vue-pluck-prototype.js
+++ vue-pluck-prototype.js
@@ -1,10 +1,21 @@
+
+// version 2
+
 module.exports = {
 
     //
     install(Vue, options) {
 
         //
-        Vue.prototype.$pluck = (array, value, key = null) => {
+        Vue.prototype.$pluck = (array_object, value, key = null) => {
+
+            // if is an object cast it
+            if (typeof array_object == 'object') {
+
+                // make array
+                array = Object.values(array_object)
+            }
+
             if (key != null) {
                 // last character will be used as init, in this case an object. init will be set in acc wish is first param
                 return array.reduce(function(o, val) { o[val[key]] = val[value]; return o; }, {});
